4f285a6fec7eafa2cb09924170886bed88922177
nhithai <nhithai@lecle.co.kr>
2023-09-25 15:51:39 +0700

    feat/about-page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,38 @@
+import { lazy } from "react";
+import IntroContent from "../../content/IntroContent.json";
+import ProductsContent from "../../content/ProductsContent.json";
+import SolutionContent from "../../content/SolutionContent.json";
+
+const IntroBlock = lazy(() => import("../../shared/components/IntroBlock"));
+const ProductsBlock = lazy(() => import("../../shared/components/ProductsBlock"));
+const SolutionBlock = lazy(() => import("../../shared/components/SolutionBlock"));
+
+const About = () => {
+  return (
+    <>
+      <IntroBlock
+        title={IntroContent.title}
+        button={IntroContent.button}
+        id="intro"
+      />
+      <ProductsBlock
+        title={ProductsContent.title}
+        text={ProductsContent.text}
+        section={ProductsContent.section}
+        button={ProductsContent.button}
+        id="products"
+      />
+ 
+      <SolutionBlock
+        title={SolutionContent.title}
+        text={SolutionContent.text}
+        section={SolutionContent.section}
+        id="solution"
+      />
+     <livere-comment client-id="9TdXiy9Vk9NTePPebJYP"></livere-comment>
+    
+    </>
+  );
+};
+
+export default About;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Footer from "../shared/components/Footer";
 import Header from "../shared/components/Header";
 
 const Home = lazy(() => import("../pages/Home"));
+const About = lazy(() => import("../pages/About"));
 
 export const Router = () => {
   return (
@@ -12,6 +13,7 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Suspense>
